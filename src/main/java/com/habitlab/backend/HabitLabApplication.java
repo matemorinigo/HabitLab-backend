c8d3e880fbb93bed80f9b13ca3edf57e2e8d02d6
Mateo <matemorinigo@gmail.com>
2024-10-29 13:38:11 -0300

    initial commit

---
+++ src/main/java/com/habitlab/backend/HabitLabApplication.java
@@ -0,0 +1,13 @@
+package com.habitlab.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habitlab/backend/HabitLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitlab.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
